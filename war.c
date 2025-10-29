8de3f7a31a254f197b57388dccf9b10ca690ddea
isaias-spec <66078452+isaias-spec@users.noreply.github.com>
2025-10-29 00:20:43 +0000

    feat: instrucoes iniciais

--- war.c
+++ war.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("WAR - Projeto\n");
+    printf("========================================\n");
+    printf("BEM-VINDO AO WAR\n");
+    printf("========================================\n");
+    printf("Instrucoes:\n");
+    printf("- Nesta fase, cadastro do mapa.\n");
+    printf("- Cada territorio tem nome, cor e tropas fixas.\n");
+    printf("\nPressione ENTER para continuar.\n");
+    getchar();
     return 0;
 }
-
 
