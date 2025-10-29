fd3e5fd3e38b02898d40b7732cf035691f6d9833
isaias-spec <66078452+isaias-spec@users.noreply.github.com>
2025-10-29 00:24:58 +0000

    feat: cadastro de nome e cor

--- war.c
+++ war.c
@@ -11,19 +11,30 @@ struct Territorio {
 
 int main() {
     char nomeMapa[50];
-    struct Territorio t[QTD]; // vetor estático com 5 territórios
+    struct Territorio t[QTD];
+    int i;
 
     printf("========================================\n");
     printf("BEM-VINDO AO WAR\n");
     printf("========================================\n");
-    printf("Pressione ENTER para continuar.\n");
     getchar();
 
     printf("Nome do mapa: ");
     fgets(nomeMapa, 50, stdin);
     nomeMapa[strcspn(nomeMapa, "\n")] = '\0';
 
-    printf("Mapa: %s\n", nomeMapa);
+    for (i = 0; i < QTD; i++) {
+        printf("\nTerritorio %d\n", i + 1);
+
+        printf("Nome: ");
+        fgets(t[i].nome, 50, stdin);
+        t[i].nome[strcspn(t[i].nome, "\n")] = '\0';
+
+        printf("Cor: ");
+        fgets(t[i].cor, 20, stdin);
+        t[i].cor[strcspn(t[i].cor, "\n")] = '\0';
+    }
+
     return 0;
 }
 
