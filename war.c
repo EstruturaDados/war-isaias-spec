9ef16d991357c1d284e97668b97c5d90ed9f917b
isaias-spec <66078452+isaias-spec@users.noreply.github.com>
2025-10-29 00:21:11 +0000

    feat: leitura do nome do mapa

--- war.c
+++ war.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
+    char nomeMapa[50];
+
     printf("========================================\n");
     printf("BEM-VINDO AO WAR\n");
     printf("========================================\n");
@@ -9,6 +12,12 @@ int main() {
     printf("- Cada territorio tem nome, cor e tropas fixas.\n");
     printf("\nPressione ENTER para continuar.\n");
     getchar();
+
+    printf("Nome do mapa: ");
+    fgets(nomeMapa, 50, stdin);
+    nomeMapa[strcspn(nomeMapa, "\n")] = '\0';
+
+    printf("Mapa: %s\n", nomeMapa);
     return 0;
 }
 
